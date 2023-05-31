6dd6f54584dbad23b760287c76d487919baf5b1a
Willian Matiussi <willianmatiussi23@gmail.com>
2023-05-31 15:05:32 -0400

    peao

--- public/script.js
+++ public/script.js
@@ -1,61 +1,76 @@
 const socket = io();
 
 function gerarTabuleiro() {
-    const tabuleiro = document.querySelector('.chessboard');
+  const tabuleiro = document.querySelector('.chessboard');
+  tabuleiro.innerHTML = '';
+  const casas = 64;
+  const tamanhoQuadrado = 50;
 
-    const casas = 64;
-    const tamanhoQuadrado = 50;
+  for (let i = 0; i < casas; i++) {
+    const quadrado = document.createElement('div');
 
-    for (let i = 0; i < casas; i++) {
-        const quadrado = document.createElement('div');
+    const linha = Math.floor(i / 8);
+    const coluna = i % 8;
 
-        const linha = Math.floor(i / 8);
-        const coluna = i % 8;
+    const cor = (linha + coluna) % 2 === 0 ? 'white' : 'black';
+    quadrado.classList.add(cor);
 
-        const cor = (linha + coluna) % 2 === 0 ? 'white' : 'black';
-        quadrado.classList.add(cor);
+    quadrado.style.width = tamanhoQuadrado + 'px';
+    quadrado.style.height = tamanhoQuadrado + 'px';
+    quadrado.id = `${8 - linha}-${coluna + 1}`;
 
-        quadrado.style.width = tamanhoQuadrado + 'px';
-        quadrado.style.height = tamanhoQuadrado + 'px';
-        quadrado.id = `${8 - linha}-${coluna + 1}`;
+    quadrado.addEventListener('click', selecionaCasa);
 
-        quadrado.addEventListener('click', selecionaCasa)
+    tabuleiro.appendChild(quadrado);
+  }
 
-        tabuleiro.appendChild(quadrado);
-    }
-
-    document.body.appendChild(tabuleiro);
+  document.body.appendChild(tabuleiro);
 }
 
-// Chamando a função para gerar o tabuleiro
-gerarTabuleiro();
+function colocarPecasNoTabuleiro(pecas) {
+  const tabuleiro = document.getElementsByClassName('chessboard')[0];
+  const quadrados = tabuleiro.getElementsByTagName('div');
 
-
-function colocarPecasNoTabuleiro() {
-    const tabuleiro = document.getElementsByClassName('chessboard')[0];
-    const quadrados = tabuleiro.getElementsByTagName('div');
-
-    const pecas = [
-        '♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
-        '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
-        '', '', '', '', '', '', '', '',
-        '', '', '', '', '', '', '', '',
-        '', '', '', '', '', '', '', '',
-        '', '', '', '', '', '', '', '',
-        '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙',
-        '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'
-    ];
-
-    for (let i = 0; i < quadrados.length; i++) {
-        quadrados[i].innerHTML = pecas[i];
-    }
+  for (let i = 0; i < quadrados.length; i++) {
+    quadrados[i].innerHTML = pecas[i];
+  }
 }
 
-function selecionaCasa(){
-    const posicao = this.id.split('-').map(v => Number(v));
-    const peca = this.innerHTML;
-    socket.emit('movimento', {posicao, peca});
+function marcarCasasDePossiveisMovimentos(posicao, movimentos) {
+  if (movimentos.length === 0) {
+    const id = posicao.toString().replace(',', '-');
+
+    document.getElementById(id).classList.add('nenhum-movimento');
+  } else {
+    movimentos.forEach(movimento => {
+      const id = movimento.toString().replace(',', '-');
+
+      document.getElementById(id).classList.add('possivel-movimento');
+    });
+
+    const id = posicao.toString().replace(',', '-');
+
+    document.getElementById(id).classList.add('selecionado');
+  }
 }
 
+function selecionaCasa() {
+  const posicao = this.id.split('-').map(v => Number(v));
+  socket.emit('movimento', posicao);
+}
 
-colocarPecasNoTabuleiro();
+socket.on('inicio', tabuleiro => {
+  gerarTabuleiro();
+  colocarPecasNoTabuleiro(tabuleiro);
+});
+
+socket.on('possiveis-movimentos', ({ movimentos, tabuleiro, posicao }) => {
+  gerarTabuleiro();
+  colocarPecasNoTabuleiro(tabuleiro);
+  marcarCasasDePossiveisMovimentos(posicao, movimentos);
+});
+
+socket.on('movimentou', tabuleiro => {
+  gerarTabuleiro();
+  colocarPecasNoTabuleiro(tabuleiro);
+});
--- server.js
+++ server.js
@@ -3,169 +3,338 @@ const path = require('path');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
-let piece = '';
+const pecaSelecionada = {
+  posicao: [],
+  possiveisMovimentos: [],
+  movimentar: posicao => {}
+};
 
-const pecasPretas = [
-    '♜', '♞', '♝', '♛', '♚', '♟'
-]
+const pecasPretas = ['♜', '♞', '♝', '♛', '♚', '♟'];
 
-const pecasBrancas = [
-    '♙', '♖', '♘', '♗', '♕', '♔'
-]
+const pecasBrancas = ['♙', '♖', '♘', '♗', '♕', '♔'];
 
 let tabuleiro = [
-    '♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
-    '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    '', '♟', '', '', '', '', '', '',
-    '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙',
-    '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'
-]
+  '♜',
+  '♞',
+  '♝',
+  '♛',
+  '♚',
+  '♝',
+  '♞',
+  '♜',
+  '♟',
+  '♟',
+  '♟',
+  '♟',
+  '♟',
+  '♟',
+  '♟',
+  '♟',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '♙',
+  '♙',
+  '♙',
+  '♙',
+  '♙',
+  '♙',
+  '♙',
+  '♙',
+  '♖',
+  '♘',
+  '♗',
+  '♕',
+  '♔',
+  '♗',
+  '♘',
+  '♖'
+];
 
-function calcIndex(linha, coluna){
-    return (64 - ((linha * 8) - coluna) + 1)
+function calcIndex(l, c) {
+  const linha = 8 - l;
+  const coluna = c;
+  return linha * 8 + coluna - 1;
 }
 
-function reset(){
-    tabuleiro = [
-        '♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
-        '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
-        '', '', '', '', '', '', '', '',
-        '', '', '', '', '', '', '', '',
-        '', '', '', '', '', '', '', '',
-        '', '', '', '', '', '', '', '',
-        '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙',
-        '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'
-    ]
+function reset() {
+  const tabuleiroInicial = [
+    '♜',
+    '♞',
+    '♝',
+    '♛',
+    '♚',
+    '♝',
+    '♞',
+    '♜',
+    '♟',
+    '♟',
+    '♟',
+    '♟',
+    '♟',
+    '♟',
+    '♟',
+    '♟',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '♙',
+    '♙',
+    '♙',
+    '♙',
+    '♙',
+    '♙',
+    '♙',
+    '♙',
+    '♖',
+    '♘',
+    '♗',
+    '♕',
+    '♔',
+    '♗',
+    '♘',
+    '♖'
+  ];
+
+  tabuleiro = tabuleiroInicial;
 }
 
-function pecaBranca(peca){
-    for(let i = 0 ; i < pecasBrancas ; i++){
-        if (pecasBrancas[i] === peca){
-            return true;
+function pecaBranca(peca) {
+  for (let i = 0; i < pecasBrancas.length; i++) {
+    if (pecasBrancas[i] === peca) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function pecaPreta(peca) {
+  for (let i = 0; i < pecasPretas.length; i++) {
+    if (pecasPretas[i] === peca) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function verificarPeca(posicao) {
+  const peca = tabuleiro[calcIndex(posicao[0], posicao[1])];
+
+  if (peca === '♙' || peca === '♟') {
+    pecaSelecionada.possiveisMovimentos = Peao.possiveisMovimentos(posicao);
+    pecaSelecionada.posicao = posicao;
+    pecaSelecionada.movimentar = Peao.movimento;
+
+    return pecaSelecionada.possiveisMovimentos;
+  } else if (peca === '') {
+    pecaSelecionada.movimentar = posicao => {};
+    pecaSelecionada.posicao = [];
+    pecaSelecionada.possiveisMovimentos = [];
+  }
+}
+
+class Peca {
+  static movimento(posicao) {
+    const [linhaAtual, colunaAtual] = pecaSelecionada.posicao;
+    const [linha, coluna] = posicao;
+    const peca = tabuleiro[calcIndex(linhaAtual, colunaAtual)];
+
+    tabuleiro[calcIndex(linhaAtual, colunaAtual)] = '';
+    tabuleiro[calcIndex(linha, coluna)] = peca;
+  }
+}
+
+class Peao extends Peca {
+  static possiveisMovimentos(posicao) {
+    const [linha, coluna] = posicao;
+    const movimentos = [];
+    if (tabuleiro[calcIndex(linha, coluna)] === '♙') {
+      if (linha === 2) {
+        for (let i = 1; i <= 2; i++) {
+          if (tabuleiro[calcIndex(linha + i, coluna)] === '') {
+            movimentos.push([linha + i, coluna]);
+          } else {
+            break;
+          }
         }
-    }
-
-    return false;
-}
-
-function pecaPreta(peca){
-    for(let i = 0 ; i < pecasPretas.length ; i++){
-        if (pecasPretas[i] === peca){
-            return true;
+      } else {
+        if (tabuleiro[calcIndex(linha + 1, coluna)] === '') {
+          movimentos.push([linha + 1, coluna]);
         }
-    }
-
-    return false;
-}
-
-class Peao{
-    movimento(){
-        if('♙'){
-
+      }
+      if (coluna - 1 > 0) {
+        if (pecaPreta(tabuleiro[calcIndex(linha + 1, coluna - 1)]))
+          movimentos.push([linha + 1, coluna - 1]);
+      }
+      if (coluna + 1 <= 8) {
+        if (pecaPreta(tabuleiro[calcIndex(linha + 1, coluna + 1)]))
+          movimentos.push([linha + 1, coluna + 1]);
+      }
+    } else {
+      if (linha === 7) {
+        for (let i = 1; i <= 2; i++) {
+          if (tabuleiro[calcIndex(linha - i, coluna)] === '') {
+            movimentos.push([linha - i, coluna]);
+          } else {
+            break;
+          }
         }
-        else{
-
+      } else {
+        if (tabuleiro[calcIndex(linha - 1, coluna)] === '') {
+          movimentos.push([linha - 1, coluna]);
         }
+      }
+      if (coluna - 1 > 0) {
+        if (pecaBranca(tabuleiro[calcIndex(linha - 1, coluna - 1)]))
+          movimentos.push([linha - 1, coluna - 1]);
+      }
+      if (coluna + 1 <= 8) {
+        if (pecaBranca(tabuleiro[calcIndex(linha - 1, coluna + 1)]))
+          movimentos.push([linha - 1, coluna + 1]);
+      }
     }
 
-    static possiveisMovimentos({ posicao }){
-        const [linha, coluna] = posicao;
-        const movimentos = [];
-        if('♙'){
-            if(linha === 2){
-                for (let i = 1 ; i <= 2 ; i++){
-                    if (tabuleiro[calcIndex(linha + i, coluna)] === ''){
-                        movimentos.push([linha + i,coluna])
-                    }
-                    else{
-                        break;
-                    }
-                }
-                if (pecaPreta(tabuleiro[calcIndex(linha + 1, coluna + 1)])) movimentos.push([linha + 1, coluna + 1]);
-                if (pecaPreta(tabuleiro[calcIndex(linha + 1, coluna - 1)])) movimentos.push([linha + 1, coluna - 1]);
-            }
-            else{
-
-            }
-        }
-        else{
-            
-        }
-
-        return movimentos;
-    }
+    return movimentos;
+  }
 }
 
-class Torre{
-    movimento(){
+class Torre {
+  movimento() {}
 
-    }
-
-    possiveisMovimentos(){
-        
-    }
+  possiveisMovimentos() {}
 }
 
-class Cavalo{
-    movimento(){
+class Cavalo {
+  movimento() {}
 
-    }
-
-    possiveisMovimentos(){
-        
-    }
+  possiveisMovimentos() {}
 }
 
-class Bispo{
-    movimento(){
+class Bispo {
+  movimento() {}
 
-    }
-
-    possiveisMovimentos(){
-        
-    }
+  possiveisMovimentos() {}
 }
 
-class Rainha{
-    movimento(){
+class Rainha {
+  movimento() {}
 
-    }
-
-    possiveisMovimentos(){
-        
-    }
+  possiveisMovimentos() {}
 }
 
-class Rei{
-    movimento(){
+class Rei {
+  movimento() {}
 
-    }
-
-    possiveisMovimentos(){
-        
-    }
+  possiveisMovimentos() {}
 }
 
-function move({ posicao,  peca}){
-    
+function checarMovimento(posicao) {
+  for (let i = 0; i < pecaSelecionada.possiveisMovimentos.length; i++) {
+    if (
+      pecaSelecionada.possiveisMovimentos[i].toString() === posicao.toString()
+    ) {
+      return true;
+    }
+  }
+
+  console.log('false');
+
+  return false;
 }
 
-io.on('connection', (socket) => {
-    socket.on('movimento', (msg) => {
-        console.log(Peao.possiveisMovimentos(msg))
-    })
+io.on('connection', socket => {
+  reset();
+
+  socket.on('movimento', posicao => {
+    if (pecaSelecionada.possiveisMovimentos.length === 0) {
+      const movimentos = verificarPeca(posicao);
+
+      socket.emit('possiveis-movimentos', { movimentos, tabuleiro, posicao });
+    } else {
+      if (checarMovimento(posicao)) {
+        pecaSelecionada.movimentar(posicao);
+        pecaSelecionada.movimentar = posicao => {};
+        pecaSelecionada.posicao = [];
+        pecaSelecionada.possiveisMovimentos = [];
+        socket.emit('movimentou', tabuleiro);
+      } else {
+        const movimentos = verificarPeca(posicao);
+
+        socket.emit('possiveis-movimentos', { movimentos, tabuleiro, posicao });
+      }
+    }
+  });
+
+  socket.emit('inicio', tabuleiro);
 });
 
 app
-.use(express.static(path.join(__dirname, 'public')))
-.use(express.urlencoded({extended: true}))
-.get('/', (req, res) => {
-    return res.sendFile('index.html', {root: __dirname})
-})
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({ extended: true }))
+  .get('/', (req, res) => {
+    return res.sendFile('index.html', { root: __dirname });
+  });
 server.listen(8000);
